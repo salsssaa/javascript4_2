e4dffc45002aaefbca2ae0c0445db0ec5cc49048
Krystian Nowak <177132@o365.prz.edu.pl>
2025-11-18 08:39:34 +0000

    4_2

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
+'use strict';
 const express = require('express');
 const app = express();
 
-// define endpoint for exercise 4 here
-app.get('/hello/name', (req, res) => {
-  const { first, last } = req.query;
+let categories = ['funnyJoke', 'lameJoke'];
 
-  // check for required parameters
-  if (!first || !last) {
-    const missingParams = [];
-    if (!first) {
-      missingParams.push('first');
-    }
-    if (!last) {
-      missingParams.push('last');
-    }
-    const errorMessage = `Missing Required GET parameters: ${missingParams.join(', ')}`;
-    return res.status(400).send(errorMessage);
-  }
+let funnyJoke = [
+  { 'joke': 'Dlaczego komputer poszedł do lekarza?', 'response': 'Bo złapał wirusa!' },
+  { 'joke': 'Dlaczego komputer nie może być głodny?', 'response': 'Bo ma pełen dysk!' },
+  { 'joke': 'Co mówi jeden bit do drugiego?', 'response': '„Trzymaj się, zaraz się przestawiamy!”' }
+];
 
-  // send greeting
-  const message = `Hello ${first} ${last}`;
-  res.type('text').send(message);
+let lameJoke = [
+  { 'joke': 'Dlaczego programiści preferują noc?', 'response': 'Bo w nocy jest mniej bugów do łapania!' },
+  { 'joke': 'Jak nazywa się bardzo szybki programista?', 'response': 'Błyskawiczny kompilator!' }
+];
+
+app.get('/jokebook/categories', (req, res) => {
+  res.json(categories);
 });
 
-// start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.get('/jokebook/joke/:category', (req, res) => {
+  const category = req.params.category;
+
+  let jokesArray;
+  if (category === 'funnyJoke') jokesArray = funnyJoke;
+  else if (category === 'lameJoke') jokesArray = lameJoke;
+  else return res.status(400).json({ error: `no jokes for category ${category}` });
+
+  const randomIndex = Math.floor(Math.random() * jokesArray.length);
+  res.json(jokesArray[randomIndex]);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
